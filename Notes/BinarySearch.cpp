a7d545f93364c1cacceeccec9fa4f5cd5b7ecf60
gauravnv16 <114969570+gauravnv16@users.noreply.github.com>
2022-10-05 09:18:24 +0530

    Initial commit

---
+++ Notes/BinarySearch.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool Binary_Search(int arr[],int n,int key){
+    int low = 0,high = n - 1;
+
+    while(low <= high){
+        int m = (low + high)/2;
+
+        if(arr[m] == key) return 1;
+
+        if(arr[m] > key) heigh = m - 1;
+        else{
+            low = m + 1;
+        }
+    }
+
+    return 0;
+}
+int main(){
+    return 0;
+}
